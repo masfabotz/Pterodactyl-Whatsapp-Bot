66c05d0db87bdd842606af892e912df592b4b16d
masfabotz <166241105+masfabotz@users.noreply.github.com>
2024-04-07 00:00:27 +0700

    cpsettings.js

--- cpsetting/cpsettings.js
+++ cpsetting/cpsettings.js
@@ -4,21 +4,21 @@ const chalk = require('chalk')
 global.domain = "https://" // Isi Domain mu, WEBSITE WAJIB ADA SSL
 global.apikey = '-' // Isi Apikey Pterodactyl mu
 global.capikey = '-' // Isi Apikey Pterodactyl mu
-global.creAtor = "6281234824414@s.whatsapp.net"
-global.owner = ['6281234824414']
-global.ownerNumber = ["6281234824414@s.whatsapp.net"]
-global.nomerOwner = "6281234824414"
-global.namabotnya = 'CPanBot'
-global.namaownernya = 'CPanBot'
-global.packname = 'CPanBot'
+global.creAtor = "6281318916604@s.whatsapp.net"
+global.owner = ['6281318916604']
+global.ownerNumber = ["6281318916604@s.whatsapp.net"]
+global.nomerOwner = "6281318916604"
+global.namabotnya = 'Go-Mar'
+global.namaownernya = 'Fajar'
+global.packname = 'Go-Mar'
 global.author = 'www.cpancloud.com'
 global.sessionName = 'session'
-global.email = 'admin@cpancloud.com' // Ganti dengan emailmu
+global.email = 'marrjan184@gmail.com' // Ganti dengan emailmu
 global.group = 'https://chat.whatsapp.com/'
 global.youtube = 'https://youtube.com/'
 global.website = 'https://www.cpancloud.com'
 global.github = 'https://github.com/Pann09'
-global.nomorowner = 'https://wa.me/6281224172387'
+global.nomorowner = 'https://wa.me/6281318916604'
 global.region = 'Indonesia'
 global.prefa = ['','!','.','#','-','•']
 global.krmd = 
@@ -30,7 +30,7 @@ owner: '```Owner Only Broo...```',
 group: '```Fitur Digunakan Hanya Untuk Group!!!```',
 private: '```Fitur Digunakan Hanya Untuk Private Chat!!!```',
 bot: '```Fitur Khusus Pengguna Nomor Bot!!!```',
-error: '```Error Kak, Hubungi owner 6281234824414```',
+error: '```Error Kak, Hubungi owner 6281318916604```',
 wait: '```Waittt...```'
 }
 
